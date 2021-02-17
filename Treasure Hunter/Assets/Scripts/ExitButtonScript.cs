0a28b030d1d209bbf4ffe2396bab0cf7397f2324
adhitamapatrick <adhitama@sheridancollege.ca>
2021-02-17 12:59:11 -0500

    Initial Commit & Push
    Contains main menu scene and scripts for the buttons

---
+++ Treasure Hunter/Assets/Scripts/ExitButtonScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ExitButtonScript : MonoBehaviour
+{
+    public void ExitGameplay()
+    {
+        EditorApplication.ExitPlaymode();
+        Debug.Log("Game is exiting");
+    }
+}
