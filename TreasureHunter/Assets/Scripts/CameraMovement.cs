248dd6b4ddf68542a899520a727a5e09a0f39101
orelkris <46901525+orelkris@users.noreply.github.com>
2021-02-17 18:19:41 -0500

    scripts

---
+++ TreasureHunter/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public static float m_camera_speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // camera movement
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector3(0, 0, 1 * m_camera_speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0, 0, -1 * m_camera_speed * Time.deltaTime));
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Translate(new Vector3(1 * m_camera_speed * Time.deltaTime, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Translate(new Vector3(-1 * m_camera_speed * Time.deltaTime, 0, 0));
+        }
+    }
+}
---
+++ TreasureHunter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public static float m_player_speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player collides with the treasure, the player wins! Show "Win" screen
+        if (this.GetComponent<Collider>().bounds.Contains(GameObject.Find("Treasure").transform.position))
+        {
+            Debug.Log("Win!");
+        }
+
+        // player movement
+        Vector3 movement = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += Vector3.forward;
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            movement += Vector3.back;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += Vector3.right;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            movement += Vector3.left;
+        }
+
+        this.transform.Translate(movement.normalized * m_player_speed * Time.deltaTime);
+
+        // player rotation
+        if (Input.GetKey(KeyCode.E))
+        {
+            this.transform.Rotate(new Vector3(0, 40 * m_player_speed * Time.deltaTime, 0));
+           
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            this.transform.Rotate(new Vector3(0, -40 * m_player_speed * Time.deltaTime, 0));
+        }
+
+        // on every update, make sure the height of the player is always on the surface of the terrain
+        // by matching the height of the terrain at the x and z position of the player (+3)
+        Vector3 newPlayerLocation = this.transform.position;
+        newPlayerLocation.y = Terrain.activeTerrain.SampleHeight(this.transform.position) + 3;
+        this.transform.position = newPlayerLocation;
+    }
+}
